0269919472b8560bd4138c846677ddeba6a2ea51
AlbertAndrea <92324558+AlbertAndrea@users.noreply.github.com>
2021-12-10 21:36:08 +0100

    partial solution 1

---
+++ bs-feladatok-checkout/main.js
@@ -0,0 +1,6 @@
+const arr1 = ["Budapest", "Baranya", "Bács-Kiskun", "Békés", "Borsod-Abaúj-Zemplén", "Csongrád", "Fejér", "Győr-Moson-Sopron", "Hajdú-Bihar", Heves, 
+    "Jász-Nagykun-Szolnok", "Komárom-Esztergom", "Nógrád", "Pest", "Somogy", "Szabolcs-Szatmár-Bereg", "Tolna", "Vas", "Veszprém", "Zala"];
+
+const arr2 = ["Utah", "New York", "Oregon"]
+
+
