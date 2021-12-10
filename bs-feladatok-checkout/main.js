ace307c35571efee1d301149c03a29cde49cfa09
AlbertAndrea <92324558+AlbertAndrea@users.noreply.github.com>
2021-12-10 22:30:58 +0100

    solved

--- bs-feladatok-checkout/main.js
+++ bs-feladatok-checkout/main.js
@@ -1,6 +1,21 @@
-const arr1 = ["Budapest", "Baranya", "Bács-Kiskun", "Békés", "Borsod-Abaúj-Zemplén", "Csongrád", "Fejér", "Győr-Moson-Sopron", "Hajdú-Bihar", Heves, 
-    "Jász-Nagykun-Szolnok", "Komárom-Esztergom", "Nógrád", "Pest", "Somogy", "Szabolcs-Szatmár-Bereg", "Tolna", "Vas", "Veszprém", "Zala"];
+const arr0 = ["Choose"];
+const arr1 = ["Choose", "Budapest", "Baranya", "Bács-Kiskun", "Békés", "Borsod-Abaúj-Zemplén", "Csongrád", "Fejér", "Győr-Moson-Sopron", "Hajdú-Bihar", "Heves", "Jász-Nagykun-Szolnok", "Komárom-Esztergom", "Nógrád", "Pest", "Somogy", "Szabolcs-Szatmár-Bereg", "Tolna", "Vas", "Veszprém", "Zala"];
+const arr2 = ["Choose", "Alabama", "Alaszka", "Arizona", "Arkansas", "Colorado", "Connecticut", "Delaware", "Dél-Dakota", "Dél-Karolina","Észak-Dakota", "Észak-Karolina", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana"];
 
-const arr2 = ["Utah", "New York", "Oregon"]
+const arr3 = [arr0, arr1, arr2];
 
+const setOptions = (select, options) => {
+        select.options.length = 0;
+        let index=0;
+        for(let i in options){
+                select.options[index++] = new Option(options[i],i);
+        }
+}
 
+const modSelect = () => {
+        let country = document.getElementById('country');
+        let selindex = country.selectedIndex;
+        setOptions(document.getElementById('state'), arr3[selindex]);
+}
+
+document.getElementById('country').onchange = modSelect;
